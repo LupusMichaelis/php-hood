94c805947be4d29cfa9a221aff2c82362715dd06
Mickaël Wolff <mickael@lupusmic.org>
2021-05-16 00:24:26 +0200

    Isolate app state

--- public_html/hood.js
+++ public_html/hood.js
@@ -1,46 +1,73 @@
-const hood = (state) =>
+const toggle_out = (offset, className) =>
+  ( element, position) =>
+    (
+      ( position === offset && element.classList.contains(className) )
+        ||
+      ( position !== offset && !element.classList.contains(className) )
+    ) && element.classList.toggle(className);
+
+const toggle_in = (offset, className) =>
+  ( element, position) =>
+    (
+      ( position !== offset && element.classList.contains(className) )
+        ||
+      ( position === offset && !element.classList.contains(className) )
+    ) && element.classList.toggle(className);
+
+const indexOfPage = (state, page_name) =>
+    state.page_list.indexOf(state.current_page);
+
+const hood = (initial_state) =>
 {
-  const iframes = document.querySelectorAll('iframe');
-  const handles = document.querySelectorAll('li.handle');
+  const set_state = (new_state) =>
+  {
+    const state = {...initial_state, ...new_state};
+    render(state);
+  };
 
-  const toggle_out = (offset, className) =>
-    ( element, position) =>
-      (
-        ( position === offset && element.classList.contains(className) )
-          ||
-        ( position !== offset && !element.classList.contains(className) )
-      ) && element.classList.toggle(className);
+  const render = (state) =>
+  {
+    const iframes = document.querySelectorAll('iframe');
+    const handles = document.querySelectorAll('li.handle');
 
-  const toggle_in = (offset, className) =>
-    ( element, position) =>
-      (
-        ( position !== offset && element.classList.contains(className) )
-          ||
-        ( position === offset && !element.classList.contains(className) )
-      ) && element.classList.toggle(className);
+    iframes.forEach(toggle_out(indexOfPage(state), 'hidden'));
+    handles.forEach(toggle_in(indexOfPage(state), 'selected'));
+  }
 
   const selectSection = (element, position) =>
   {
+    const iframes = document.querySelectorAll('iframe');
+
     element.onclick = () =>
-    {
-      iframes.forEach(toggle_out(position, 'hidden'));
-      handles.forEach(toggle_in(position, 'selected'));
-      state.current_page = state.page_list[position];
-    }
+      set_state({current_page: initial_state.page_list[position]});
 
     element.ondblclick = () =>
       iframes[position].src += '';
   };
 
   const reloadSection = (element, position) =>
+  {
+    const iframes = document.querySelectorAll('iframe');
+
     element.querySelectorAll('i.reloader').forEach( (e) => e.onclick =
       () => iframes[position].src += '');
+  }
 
   const inspectSection = (element, position) =>
+  {
+    const iframes = document.querySelectorAll('iframe');
     element.querySelectorAll('i.inspector').forEach( (e) => e.onclick =
       () => window.open(iframes[position].src, '_blank'));
+  }
 
-  handles.forEach(selectSection);
-  handles.forEach(reloadSection);
-  handles.forEach(inspectSection);
+  (() =>
+  {
+    const handles = document.querySelectorAll('li.handle');
+    handles.forEach(selectSection);
+    handles.forEach(reloadSection);
+    handles.forEach(inspectSection);
+  }
+  )();
 };
+
+window.hood = hood;

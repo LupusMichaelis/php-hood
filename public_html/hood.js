dadd732e2d889ffa6d10ab4a5cdd73c3f1321f55
Mickaël Wolff <mickael@lupusmic.org>
2021-05-16 00:56:01 +0200

    Reorganize to get out all non-state bound functions out of hood main

--- public_html/hood.js
+++ public_html/hood.js
@@ -17,22 +17,41 @@ const toggle_in = (offset, className) =>
 const indexOfPage = (state, page_name) =>
     state.page_list.indexOf(state.current_page);
 
-const hood = (initial_state) =>
+
+const inspectSection = (element, position) =>
 {
+  const iframes = document.querySelectorAll('iframe');
+  element.querySelectorAll('i.inspector').forEach( (e) => e.onclick =
+    () => window.open(iframes[position].src, '_blank'));
+}
+
+const reloadSection = (element, position) =>
+{
+  const iframes = document.querySelectorAll('iframe');
+
+  element.querySelectorAll('i.reloader').forEach( (e) => e.onclick =
+    () => iframes[position].src += '');
+}
+
+const check_state = (given_state) =>
+{
+  const virgin_state =
+  { current_page: null
+  , page_list: []
+  };
+
+  return {...virgin_state, ...given_state};
+};
+
+const hood = (given_state) =>
+{
+  const initial_state = check_state(given_state);
+
   const set_state = (new_state) =>
   {
     const state = {...initial_state, ...new_state};
     render(state);
   };
-
-  const render = (state) =>
-  {
-    const iframes = document.querySelectorAll('iframe');
-    const handles = document.querySelectorAll('li.handle');
-
-    iframes.forEach(toggle_out(indexOfPage(state), 'hidden'));
-    handles.forEach(toggle_in(indexOfPage(state), 'selected'));
-  }
 
   const selectSection = (element, position) =>
   {
@@ -45,29 +64,20 @@ const hood = (initial_state) =>
       iframes[position].src += '';
   };
 
-  const reloadSection = (element, position) =>
+  const render = (state) =>
   {
     const iframes = document.querySelectorAll('iframe');
+    iframes.forEach(toggle_out(indexOfPage(state), 'hidden'));
 
-    element.querySelectorAll('i.reloader').forEach( (e) => e.onclick =
-      () => iframes[position].src += '');
-  }
-
-  const inspectSection = (element, position) =>
-  {
-    const iframes = document.querySelectorAll('iframe');
-    element.querySelectorAll('i.inspector').forEach( (e) => e.onclick =
-      () => window.open(iframes[position].src, '_blank'));
-  }
-
-  (() =>
-  {
     const handles = document.querySelectorAll('li.handle');
+    handles.forEach(toggle_in(indexOfPage(state), 'selected'));
+
     handles.forEach(selectSection);
     handles.forEach(reloadSection);
     handles.forEach(inspectSection);
-  }
-  )();
+  };
+
+  render(initial_state);
 };
 
 window.hood = hood;

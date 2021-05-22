8bee7112e468bc917e948f16abe49ad96157b92a
Mickaël Wolff <mickael@lupusmic.org>
2021-05-23 00:13:57 +0200

    Move templates and app logic out of index

---
+++ src/App.php
@@ -0,0 +1,248 @@
+<?php declare(strict_types=1);
+
+namespace LupusMichaelis\PHPHood;
+
+class App
+{
+	const suffix_to_mime_list =
+		[ 'html' => 'text/html'
+		, 'json' => 'application/json'
+		];
+
+	public function run(): void
+	{
+		$config = $this->getConfiguration();
+
+		if(isset($_GET['page']))
+		{
+			$page = $_GET['page'];
+			if(isset($this->page_list[$page]['controller']))
+				$this->page_list[$page]['controller']($this);
+			else
+				$this->errors[] = sprintf('App \'%s\' not supported', $page);
+		}
+
+		include $this->getTemplateFor('index');
+	}
+
+	public function getTemplateFor($name, $type='html'): ?string
+	{
+		if(!isset(self::suffix_to_mime_list[$type]))
+		{
+			$this->errors[] = sprintf('Can\'t find requested filetype (%s|%s)', $type);
+			return null;
+		}
+
+		$config = $this->getConfiguration();
+		return sprintf
+			( '%s/%s.%s.php'
+			, $config->getTemplatePath()
+			, $name
+			, $type
+			);
+	}
+
+	public function getConfiguration(): Config
+	{
+		if(!isset($this->configuration))
+		{
+			$this->configuration = new Config;
+			$this->configuration->bindErrors($this->errors);
+		}
+
+		return $this->configuration;
+	}
+
+	public $page_list = [];
+	public $state = [];
+	private $errors;
+	private $configuration;
+
+	public function __construct()
+	{
+		$this->errors = new Errors;
+
+		$this->page_list =
+			[ 'php-info' =>
+				[ 'title' => 'PHP Infos'
+				, 'feature_list' => ['reloader']
+				, 'controller' => function(App $app)
+					{
+						phpinfo();
+						die();
+					}
+				]
+			, 'apcu-info' =>
+				[ 'title' => 'APCu Infos'
+				, 'feature_list' => ['reloader']
+				, 'controller' => function(App $app)
+					{
+						if(file_exists('apc.php'))
+						{
+							header('Location: ./apc.php');
+							die();
+						}
+
+						if(!isset($this->configuration->get()['apc']['provided-monitor']))
+						{
+							header('HTTP/1.1 404 Not found');
+							header('Content-type: text/plain');
+							die('APCu hood not configured');
+						}
+
+						$apc_scriptname = $this->configuration->get()['apc']['provided-monitor'];
+
+						$copied = @copy($apc_scriptname, 'apc.php');
+
+						if(!$copied)
+						{
+							$app->errors[] = sprintf
+								( 'Couldn\'t copy \'%s\' because \'%s\''
+								, $this->configuration->get()['apc']['provided-monitor']
+								, error_get_last()['message']
+								);
+
+							header('HTTP/1.1 404 Not found');
+							header('Content-type: text/plain');
+							die(sprintf('File \'%s\' not found', 'apc.php'));
+						}
+
+						header('Location: ./apc.php');
+						die();
+					}
+				]
+			, 'apcu-stats' =>
+				[ 'title' => 'APCu stats'
+				, 'feature_list' => ['reloader', 'inspector']
+				, 'controller' => function(App $app)
+					{
+						header('Content-type: application/json');
+
+						if(function_exists('\apcu_cache_info'))
+							die(json_encode(apcu_cache_info()));
+
+						die(json_encode(['error' => 'Function \'\\apcu_cache_info\' doesn\'t exist']));
+					}
+				]
+			, 'memcached-stats' =>
+				[ 'title' => 'Memcache infos'
+				, 'feature_list' => ['reloader', 'inspector']
+				, 'controller' => new class
+					{
+						var $app = [];
+
+						function __invoke(App $app)
+						{
+							$this->config = $app->getConfiguration()->get();
+
+							$payload = [];
+							foreach
+								(
+									[ 'memcache' => 'check_memcache'
+									, 'memcached' => 'check_memcached'
+									] as $extension => $method
+								)
+								$payload[$extension] = $this->$method();
+
+							header('Content-type: application/json');
+							die(json_encode($payload));
+						}
+
+						function check_memcache()
+						{
+							[ $is_instanciated, $connections, $stats ] = [ null, null, []];
+							$class_exists = class_exists('\Memcache');
+
+							if($class_exists)
+							{
+								$con = new \Memcache;
+								$is_instanciated = (bool) $con;
+
+								$connections = [];
+								if($con)
+								{
+									foreach($this->config['memcached'] as [$host, $port])
+									{
+										$host_port = "$host:$port";
+										$connections[$host_port] =
+											@$con->connect($host, $port)
+												? 'succeed'
+												: 'failed'
+												;
+
+										$stats[$host_port] = $con->getStats();
+									}
+
+									$con->close();
+								}
+
+								$stats[] = @$con->getStats();
+								unset($con);
+							}
+
+							return compact
+								( 'class_exists'
+								, 'is_instanciated'
+								, 'connections'
+								, 'stats'
+								);
+						}
+
+						function check_memcached()
+						{
+							[ $is_instanciated, $connections, $stats, $beacon ] = [ null, null, [], []];
+							$class_exists = class_exists('\Memcached');
+							$is_configured = isset($this->config['memcached']) ? true : false;
+
+							if($class_exists && $is_configured)
+							{
+								$con = new \Memcached;
+								$is_instanciated = (bool) $con;
+
+								$connections = [];
+								if($con)
+								{
+									foreach($this->config['memcached'] as [$host, $port])
+									{
+										$host_port = "$host:$port";
+										$connections[$host_port] =
+											@$con->addServer($host, $port)
+												? 'succeed'
+												: 'failed'
+												;
+
+										$beacon[$host_port] = $this->beacon($con);
+										$stats[$host_port] = $con->getStats();
+
+										// If an added server's failing, the whole pool is
+										// failing and no stat can be gathered
+										$con->resetServerList();
+									}
+								}
+
+								$stats[] = @$con->getStats();
+								unset($con);
+							}
+							return compact
+								( 'class_exists'
+								, 'is_configured'
+								, 'is_instanciated'
+								, 'connections'
+								, 'stats'
+								, 'beacon'
+								);
+						}
+
+						function beacon(\Memcached $con, $beacon = 0)
+						{
+							$con->add('beacon', $beacon)
+								or $beacon = $con->increment('beacon')
+								or $con->set('beacon', $beacon);
+
+							return $beacon;
+						}
+					}
+				]
+			];
+	}
+}
---
+++ templates/index.html.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types=1);
+
+if(!isset($this) && !($this instanceof \LupusMichaelis\PHPHood\Page))
+	throw new \LupusMichaelis\PHPHood\TemplateContextError;
+
+$app = $this;
+$state = &$this->state;
+$page_list = &$this->page_list;
+$errors = $this->errors;
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Under the hood of <?= htmlentities($_SERVER['SERVER_NAME']) ?></title>
+    <meta name='viewport' content='width=device-width' />
+    <link rel='stylesheet'
+          type='text/css'
+          href='hood.css'
+          defer
+          />
+    <script type='application/javascript'>
+      const state = <?= json_encode($state) ?>;
+      window.addEventListener('load', () => { hood(state); });
+    </script>
+  </head>
+  <body>
+    <nav>
+      <ol>
+<?php foreach($page_list as $page_id => $page_config): ?>
+        <li id='<?= htmlentities($page_id, ENT_QUOTES) ?>'
+            class='handle
+<?php   if($state['current_page'] === $page_id): ?>
+                   selected
+<?php   endif ?>
+            '
+            ><a href='?current=<?= htmlentities($page_id) ?>'>
+              <i class='remover'>&cross;</i>
+              <?= htmlentities($page_config['title']) ?>
+<?php   if(!empty($page_config['feature_list'])): ?>
+<?php     foreach($page_config['feature_list'] as $feature): ?>
+<?php       if('reloader' === $feature): ?>
+              <i class='reloader'>&#128472;</i>
+<?php       endif ?>
+<?php       if('inspector' === $feature): ?>
+          <i class='inspector'>&neArr;</i>
+<?php       endif ?>
+<?php     endforeach ?>
+<?php   endif ?>
+          </a>
+        </li>
+<?php endforeach ?>
+      </ol>
+    </nav>
+
+<?php foreach($page_list as $page_id => $page_config): ?>
+    <iframe src='?page=<?= htmlentities($page_id, ENT_QUOTES) ?>'
+<?php   if(@$$state['current_page'] !== $page_id): ?>
+            class='hidden'
+<?php   endif ?>
+            ></iframe>
+<?php endforeach ?>
+
+    <div class='hidden'>
+      <script defer
+              type='application/javascript'
+              src='hood.js'
+              ></script>
+    </div>
+<?php if(count($errors)): ?>
+    <div class='error'>
+      <span>Errors occurred:</span>
+<?php   foreach($errors as $error): ?>
+      <ul>
+        <li><?= htmlentities($error) ?></li>
+      </ul>
+    </div>
+<?php   endforeach ?>
+<?php endif ?>
+  </body>
+</html>
